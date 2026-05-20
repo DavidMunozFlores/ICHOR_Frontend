ecc4dae585afc84b421d57e2f27c930334fb82dd
DavidMunozFlores <davidmunozflores05@gmail.com>
2026-05-20 12:50:44 +0200

    [uncompleted] login structuration example

---
+++ ICHOR_FrontendProyect/src/app/pages/log-in/log-in.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in',
+  imports: [],
+  templateUrl: './log-in.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LogIn {
+
+
+
+}
