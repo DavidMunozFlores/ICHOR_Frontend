81b8bd754a7f283cb91015f9acb59dae2e0ff9d4
PabloBF <65788099+WinnerPlus@users.noreply.github.com>
2026-05-22 13:02:35 +0200

    Update create-users.ts

--- ICHOR_FrontendProyect/src/app/pages/create-users/create-users.ts
+++ ICHOR_FrontendProyect/src/app/pages/create-users/create-users.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from "@angular/common";
-import { Component, OnInit } from "@angular/core";
+import { Component, EnvironmentInjector, inject, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../../environments/environment";
 
 @Component({
   templateUrl: 'create-users.html',
@@ -10,16 +12,24 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 export class createUserComponent implements OnInit {
   userForm!: FormGroup;
+  private http = inject(HttpClient);
 
-  hospitals = [
-    {id: 1, name: 'Hospital1'},
-    {id: 2, name: 'Hospital2'}
-  ];
+  hospitals: any[] = [];
+
+  loadHospitals() {
+    const url = `${environment.url}/api/v1/hospitales`;
+    this.http.get<any[]>(url).subscribe({
+      next: data => {this.hospitals = data.map(h => ({ id: h.id, name: h.name }));
+    },
+    error: (err) => console.error('Error al cargar los hospitales', err)
+    });
+  }
 
   constructor(private fb: FormBuilder){}
 
   ngOnInit(): void {
     this.initform();
+    this.loadHospitals();
   }
 
   initform() {
@@ -34,7 +44,8 @@ export class createUserComponent implements OnInit {
     });
   }
   onSubmit(): void {
-    console.log('Datos : ', this.userForm.value);
+    const { username, password, hospitals } = this.userForm.value
+    console.log('Datos : username: ' , username, 'password', password, 'id' ,hospitals);
   }
 
 
