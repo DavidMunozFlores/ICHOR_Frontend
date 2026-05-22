2fbb388e50901ce5954cfb44ea58caf83f29feda
PabloBF <65788099+WinnerPlus@users.noreply.github.com>
2026-05-22 12:00:20 +0200

    Update app.routes.ts

--- ICHOR_FrontendProyect/src/app/app.routes.ts
+++ ICHOR_FrontendProyect/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { LogIn } from './pages/log-in/log-in';
+import { createUserComponent } from './pages/create-users/create-users';
 
 export const routes: Routes = [
 
   {
     path:'log-in',
     component: LogIn
+  },
+  {
+    path:'create-users',
+    component:createUserComponent
   },
   {
      path:'',
