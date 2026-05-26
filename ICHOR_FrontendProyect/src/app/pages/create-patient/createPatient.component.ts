43f8994f973c06ac377b6f31b741e670ccc5a565
PabloBF <65788099+WinnerPlus@users.noreply.github.com>
2026-05-26 10:50:37 +0200

    Update createPatient.component.ts

--- ICHOR_FrontendProyect/src/app/pages/create-patient/createPatient.component.ts
+++ ICHOR_FrontendProyect/src/app/pages/create-patient/createPatient.component.ts
@@ -1,12 +1,50 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { ChangeDetectionStrategy, Component, inject, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { form } from "@angular/forms/signals";
+import { groupBy } from "rxjs";
 
 
 @Component({
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './createPatient.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class createPatient {
+export class createPatient implements OnInit{
 
-}
+    patientForm!: FormGroup;
+    private http = inject(HttpClient)
+
+    bloodTypes: string[] = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
+
+    constructor(private fb: FormBuilder){}
+
+    ngOnInit(): void {
+      this.initForm();
+    }
+
+    initForm(): void {
+      this.patientForm = this.fb.group({
+        fullname: [''],
+        dni: [''],
+        bloodType: ['']
+      });
+    }
+
+    onSubmit(): void {
+      if (this.patientForm.invalid) return;
+
+      const { fullname, dni, bloodType }= this.patientForm.value;
+
+      const bodyTosend = {
+        name: fullname,
+        dni: dni,
+        blood_type: bloodType
+      }
+      console.log(bodyTosend);
+    }
+
+
+  }
