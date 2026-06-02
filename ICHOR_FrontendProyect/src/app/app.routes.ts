11459a9cf8f3c8066b8bc917e185ecdd906e65c3
PabloBF <65788099+WinnerPlus@users.noreply.github.com>
2026-06-02 17:02:35 +0200

    Update app.routes.ts

--- ICHOR_FrontendProyect/src/app/app.routes.ts
+++ ICHOR_FrontendProyect/src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
+
 import { LogIn } from './pages/LogInPage/log-in';
-import { createUserComponent } from './pages/create-users/create-users';
 import { UserManager } from './pages/UserManagerPage/user-manager';
+import { createPatient } from './pages/create-patient/createPatient.component';
+import { createUserComponent } from './pages/create-users/create-users';
 import { DoctorPageComponent } from './pages/DoctorPage/DoctorPageComponent';
 import { CoordinatorPageComponent } from './pages/CoordinatorPage/CoordinatorPageComponent';
 import { authGuard } from './auth/guards/authGuard.guard';
 import { roleGuard } from './auth/guards/roleGuard.guard';
 import { UnauthorizedPage } from './pages/UnauthorizedPage/UnauthorizedPage';
-import { createPatient } from './pages/create-patient/createPatient.component';
 
 export const routes: Routes = [
 
@@ -27,4 +28,32 @@ export const routes: Routes = [
      path:'patientCreate',
     component: createPatient
   },
+  {
+    path: 'unauthorized',
+    component: UnauthorizedPage
+  },
+  {
+    path: 'create-users',
+    component: createUserComponent,
+    canActivate: [authGuard, roleGuard],
+    data: { role: ['MANAGER'] }
+  },
+  {
+    path: 'user-manager',
+    component: UserManager,
+    canActivate: [authGuard, roleGuard],
+    data: { role: ['MANAGER'] }
+  },
+  {
+    path: 'doctor-page',
+    component: DoctorPageComponent,
+    canActivate: [authGuard, roleGuard],
+    data: { role: ['DOCTOR'] }
+  },
+  {
+    path: 'coordinator-page',
+    component: CoordinatorPageComponent,
+    canActivate: [authGuard, roleGuard],
+    data: { role: ['COORDINATOR'] }
+  }
 ];
