95ae46322658622dcae266be750d8d2fb64a7262
DavidMunozFlores <davidmunozflores05@gmail.com>
2026-06-02 13:30:30 +0200

    [create]

---
+++ ICHOR_FrontendProyect/src/app/pages/OrganPetitionManagement/OrganPetitionManagement.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organ-petition-management',
+  imports: [],
+  templateUrl: './OrganPetitionManagement.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class OrganPetitionManagement {}
